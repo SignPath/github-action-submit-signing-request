c58f0fdf8ccd9d6f89ecd5a75117b1fb89d3eae3
TeamCity build process <noreply@signpath.io>
2026-04-14 14:19:40 +0200

    Build from - c2a2e867fd827ab6827cf619a300c5472747fd3e. Original commit message - Bump version '2.1.0' -> '2.2.0' by interactive release script

--- index.js
+++ index.js
@@ -14807,7 +14807,7 @@ function parseUseDefinedParameter(line) {
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.taskVersion = void 0;
-const taskVersion = '2.1.0';
+const taskVersion = '2.2.0';
 exports.taskVersion = taskVersion;
 
 
